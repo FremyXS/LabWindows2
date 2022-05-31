cb2695afd0c2cb214d51be3deb6b2a2dbbbf282b
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-05-31 15:49:41 +0500

    Add files via upload

---
+++ Console/Console.cpp
@@ -0,0 +1,43 @@
+﻿// MathClient.cpp : Client app for MathLibrary DLL.
+// #include "pch.h" Uncomment for Visual Studio 2017 and earlier
+#include <iostream>
+#include <Windows.h>
+extern "C" {
+	#include "D:\\proga\\OC\\LabWindows2\\numbers\\pch.cpp"
+}
+
+using namespace std;
+
+struct complex
+{
+	double x;
+	double y;
+};
+int main()
+{	
+	cout << "select 1 if normal num, select 2 if complex num" << endl;
+
+	int num;
+
+	cin >> num;
+
+	if (num == 1) {
+		cout << Sum(2,3) << endl;
+	}
+	if (num == 2)
+	{
+		complex(*Sum)(complex c1, complex c2);
+		HMODULE hModule;
+		hModule = LoadLibrary(L"c_numbers.dll");
+		Sum = reinterpret_cast<complex(*)(complex, complex)>(GetProcAddress(hModule, "Sum"));
+		complex c1{ 1,2 };
+		complex c2{ 2,2 };
+		complex res = Sum(c1, c2);
+		std::cout << res.x << std::endl << res.y << std::endl;
+	}
+
+	return 0;
+	
+}
+
+	
---
+++ c_numbers/pch.cpp
@@ -0,0 +1,45 @@
+﻿// pch.cpp: файл исходного кода, соответствующий предварительно скомпилированному заголовочному файлу
+
+#include "pch.h"
+// При использовании предварительно скомпилированных заголовочных файлов необходим следующий файл исходного кода для выполнения сборки.
+
+
+//typedef struct _complex
+//{
+//	double x;
+//	double y;
+//}complex;
+
+complex Sum(complex c1, complex c2) {
+	
+	double x = c1.x + c2.x;
+	double y = c1.y + c2.y;
+	complex c;
+	c.x = x;
+	c.y = y;
+	return c;
+}
+complex Min(complex c1, complex c2) {
+
+	double x = c1.x - c2.x;
+	double y = c1.y - c2.y;
+	complex c;
+	c.x = x;
+	c.y = y;
+	return c;
+}
+complex Mult(complex c1, complex c2) {
+
+	double x = (c1.x * c2.x) + (c1.x * c2.y);
+	double y = (c1.y * c2.x) + (c1.y * c2.y);;
+	complex c;
+	c.x = x;
+	c.y = y;
+	return c;
+}
+//extern "C" __declspec(dllexport) struct complex Min(struct complex c1, struct complex c2){
+//	return c1;
+//}
+//extern "C" __declspec(dllexport) struct complex Mult(struct complex c1, struct complex c2) {
+//	return c1;
+//}
